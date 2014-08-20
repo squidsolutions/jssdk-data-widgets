9d361e09c6571d50aa00cbabbbcd142ea33e8685
obalbous <oba@squid>
2014-08-20 12:12:02 +0200

    added kpi widget

---
+++ src/squid_api_kpi_widget.js
@@ -0,0 +1,58 @@
+(function (root, factory) {
+    root.squid_api.view.KPIView = factory(root.Backbone, root.squid_api);
+}(this, function (Backbone, squid_api) {
+
+    View = Backbone.View.extend( {
+
+        template : null,
+        
+        format : null,
+
+        initialize : function(options) {
+            if (this.model) {
+                this.model.on('change', this.render, this);
+            }
+            // setup options
+            if (options.template) {
+                this.template = options.template;
+            } else {
+                this.template = squid_api.template.squid_api_kpi_widget;
+            }
+            if (options.format) {
+                this.format = options.format;
+            } else {
+                this.format = function(f){return f;};
+            }
+        },
+
+        setModel : function(model) {
+            this.model = model;
+            this.initialize();
+        },
+
+        render : function() {
+            var jsonData, results, values;
+            if (this.model.isDone()) {
+                jsonData = {};
+                jsonData.done = true;
+                results = this.model.get("results");
+                if (results) {
+                    if (results.rows.length == 1) {
+                        values = results.rows[0].v;
+                        if (values.length == 2) {
+                            jsonData.value = this.format((values[1] / values[0]) * 100);
+                            jsonData.unit = "%";
+                            jsonData.name = results.cols[1].lname;
+                        }
+                    } 
+                }
+            }
+            var tableContent = this.$el;
+            var tableHTML = this.template(jsonData);
+            tableContent.html(tableHTML);
+            return this;
+        }
+    });
+
+    return View;
+}));
