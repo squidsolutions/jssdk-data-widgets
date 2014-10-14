7a30d6ce5f7bae3f46baa4381dcb9d01426c3087
obalbous <oba@squid>
2014-10-14 15:14:59 +0200

    fixed the single analysis case

--- src/squid_api_datatable_widget.js
+++ src/squid_api_datatable_widget.js
@@ -49,10 +49,8 @@
 
             // Use only the first array if multiple happen to exist
 
-            if (jsonData.analyses instanceof Array && jsonData.analyses !== null) {
-              jsonData = jsonData.analyses[0].attributes;
-            } else {
-              jsonData = jsonData.attributes;
+            if (jsonData.analyses && jsonData.analyses instanceof Array) {
+              jsonData = jsonData.analyses[0];
             }
 
             data = {};
