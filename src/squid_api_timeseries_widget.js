44b9cbe2594b17deefedd0f433f67ddcc0d05889
obalbous <oba@squid>
2014-11-13 15:29:33 +0100

    added support for null dates and did some code review

--- src/squid_api_timeseries_widget.js
+++ src/squid_api_timeseries_widget.js
@@ -80,20 +80,20 @@
             // Specify a colour return value for each metric
             switch (serie) {
 
-                case "count" :
-                    color = "#fe6e70";
+            case "count" :
+                color = "#fe6e70";
                 break;
-                case "withFTA" :
-                    color = "#67e363";
+            case "withFTA" :
+                color = "#67e363";
                 break;
-                case "max_date" :
-                    color = "#b563e2";
+            case "max_date" :
+                color = "#b563e2";
                 break;
-                case "sum_fta" :
-                    color = "#ffc46f";
+            case "sum_fta" :
+                color = "#ffc46f";
                 break;
-                case "unique_ftas" :
-                    color = "#67e363";
+            case "unique_ftas" :
+                color = "#67e363";
                 break;
             }
 
@@ -102,74 +102,65 @@
 
         seriesDataValues : function(serie, index, modelData) {
             var currentIndex = index;
-
             var seriesData = [];
-
-            $.each(modelData, function(index, value) {
-                var object = {};
-
-                // Convert date value into unix
-                object.x = moment(value[0]).unix();
-                object.y = parseFloat(value[currentIndex + 1]);
-                seriesData.push(object);
-            });
-
+            var value, date;
+            for (var i=0; i<modelData.length; i++) {
+                value = modelData[i].v;
+                date = moment(value[0]);
+                if (date.isValid()) {
+                    var object = {};
+                    // Convert date value into unix
+                    object.x = date.unix();
+                    object.y = parseFloat(value[currentIndex + 1]);
+                    seriesData.push(object);
+                } else {
+                    console.debug("Invalid date : "+value[0]);
+                }
+            }
             return seriesData;
         },
 
         getData: function() {
 
-                var jsonData, data, rowIdx, colIdx, row, rows, v, analysis;
+            var jsonData, data, rowIdx, colIdx, row, rows, v, analysis;
 
-                analysis = this.model;
+            analysis = this.model;
 
-                // Use the first analyses array
+            // Use the first analyses array
 
-                if (analysis.get("analyses")) {
-                  analysis = analysis.get("analyses")[0];
-                }
+            if (analysis.get("analyses")) {
+                analysis = analysis.get("analyses")[0];
+            }
 
-                jsonData = analysis.toJSON();
+            jsonData = analysis.toJSON();
 
-                data = {};
-                data.done = this.model.isDone();
-                if (jsonData.results) {
-                    data.results = {"cols" : jsonData.results.cols, "rows" : []};
-                    rows = jsonData.results.rows;
-                    for (rowIdx = 0; (rowIdx<rows.length && rowIdx<this.dataToDisplay); rowIdx++) {
-                        row = rows[rowIdx];
-                        newRow = {v:[]};
-                        for (colIdx = 0; colIdx<jsonData.results.cols.length; colIdx++) {
-                            v = row.v[colIdx];
-                            if (jsonData.results.cols[colIdx].dataType == "NUMBER") {
-                                v = v;
-                            }
-                            newRow.v.push(v);
+            data = {};
+            data.done = this.model.isDone();
+            if (jsonData.results) {
+                data.results = {"cols" : jsonData.results.cols, "rows" : []};
+                rows = jsonData.results.rows;
+                for (rowIdx = 0; (rowIdx<rows.length && rowIdx<this.dataToDisplay); rowIdx++) {
+                    row = rows[rowIdx];
+                    newRow = {v:[]};
+                    for (colIdx = 0; colIdx<jsonData.results.cols.length; colIdx++) {
+                        v = row.v[colIdx];
+                        if (jsonData.results.cols[colIdx].dataType == "NUMBER") {
+                            v = v;
                         }
-                        data.results.rows.push(newRow);
+                        newRow.v.push(v);
                     }
+                    data.results.rows.push(newRow);
                 }
+            }
 
-                return data;
+            return data;
         },
 
         sortDateValues : function(dates) {
-            var data = [];
-
-            $.each(dates, function(index, value) {
-                var object = {};
-
-                $.each(value.v, function(index, value) {
-                    object[index] = value;
-                });
-
-                data.push(object);
+            dates.sort(function(a,b){
+                return (a.x - b.x);
             });
-
-            // Make sure that dates are sorted from lowest to highest
-            var sortedDates = _.sortBy(data, function(o) { return o[0]; });
-
-            return sortedDates;
+            return dates;
         },
 
         render : function() {
@@ -198,8 +189,6 @@
                     var object = {};
                     var metricName;
 
-                    $(".sq-loading").hide();
-
                     // Check ID with column data to get a human readable name
                     for (a=0; a<data.results.cols.length; a++) {
                         if (data.results.cols[a].id === metricNames[i]) {
@@ -209,13 +198,12 @@
 
                     object.color = me.seriesColorAssignment(metricNames[i]);
                     object.name = metricName;
-                    object.data = me.seriesDataValues(metricNames[i], i, me.sortDateValues(data.results.rows));
+                    object.data = me.sortDateValues(me.seriesDataValues(metricNames[i], i, data.results.rows));
 
                     // Detect if data returned has been processed properly
                     if (isNaN(object.data[0].x)) {
                         me.invalidData = true;
                         break;
-
                     } else {
                         me.invalidData = false;
                     }
@@ -228,69 +216,69 @@
 
                     // Time Series Chart
                     var graph = new Rickshaw.Graph({
-                           element: document.getElementById("chart"),
-                           width: tempWidth,
-                           height: 400,
-                           renderer: 'line',
-                              series: series
-                         });
+                        element: document.getElementById("chart"),
+                        width: tempWidth,
+                        height: 400,
+                        renderer: 'line',
+                        series: series
+                    });
 
-                         graph.render();
+                    graph.render();
 
-                         var hoverDetail = new Rickshaw.Graph.HoverDetail( {
-                             graph: graph,
-                             xFormatter: function(x) { return "Date: " + moment.utc(x, 'X').format('YYYY-MM-DD');},
-                             yFormatter: function(y) { return Math.floor(y); }
-                         });
+                    var hoverDetail = new Rickshaw.Graph.HoverDetail( {
+                        graph: graph,
+                        xFormatter: function(x) { return "Date: " + moment.utc(x, 'X').format('YYYY-MM-DD');},
+                        yFormatter: function(y) { return Math.floor(y); }
+                    });
 
-                         var legend = new Rickshaw.Graph.Legend( {
-                            graph: graph,
-                            element: document.getElementById('legend')
-                         });
+                    var legend = new Rickshaw.Graph.Legend( {
+                        graph: graph,
+                        element: document.getElementById('legend')
+                    });
 
-                         var xAxis = new Rickshaw.Graph.Axis.Time( {
-                             graph: graph
-                         });
+                    var xAxis = new Rickshaw.Graph.Axis.Time( {
+                        graph: graph
+                    });
 
-                         var yAxis = new Rickshaw.Graph.Axis.Y( {
-                             graph: graph
-                         });
+                    var yAxis = new Rickshaw.Graph.Axis.Y( {
+                        graph: graph
+                    });
 
-                         var slider = new Rickshaw.Graph.RangeSlider({
-                             graph: graph,
-                             element: document.querySelector('#slider')
-                        });
+                    var slider = new Rickshaw.Graph.RangeSlider({
+                        graph: graph,
+                        element: document.querySelector('#slider')
+                    });
 
-                        var offsetForm = document.getElementById('offset_form');
+                    var offsetForm = document.getElementById('offset_form');
 
-                        // Change chart type on button change
-                        offsetForm.addEventListener('change', function(e) {
-                            var offsetMode = e.target.value;
+                    // Change chart type on button change
+                    offsetForm.addEventListener('change', function(e) {
+                        var offsetMode = e.target.value;
 
-                            if (offsetMode == 'lines') {
-                                graph.setRenderer('line');
-                                graph.offset = 'zero';
-                            } else if (offsetMode == 'stack') {
-                                graph.setRenderer('stack');
-                                graph.offset = offsetMode;
-                            } else if (offsetMode == 'bar') {
-                                graph.setRenderer('bar');
-                                graph.offset = offsetMode;
-                            }
+                        if (offsetMode == 'lines') {
+                            graph.setRenderer('line');
+                            graph.offset = 'zero';
+                        } else if (offsetMode == 'stack') {
+                            graph.setRenderer('stack');
+                            graph.offset = offsetMode;
+                        } else if (offsetMode == 'bar') {
+                            graph.setRenderer('bar');
+                            graph.offset = offsetMode;
+                        }
 
-                            graph.render();
+                        graph.render();
 
-                        }, false);
+                    }, false);
 
-                         yAxis.render();
-                         xAxis.render();
+                    yAxis.render();
+                    xAxis.render();
 
 
-                     } else {
-                         this.$el.html("<div class='bad-data'>Time Series incompatible, please choose another</span>");
-                     }
-
-                     return this;
+                } else {
+                    this.$el.html("<div class='bad-data'>Time Series incompatible, please choose another</span>");
+                }
+                $(".sq-loading").hide();
+                return this;
             }
         }
     });
