540aaa832e43b1fbe78e1a31f5cc53148b319ef5
obalbous <oba@squid>
2014-12-12 17:09:53 +0100

    DASH-34 Fix CSS for Order/Limit button

--- dist/squid_api_data-widgets.js
+++ dist/squid_api_data-widgets.js
@@ -490,15 +490,15 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
 function program1(depth0,data) {
   
   var buffer = "", stack1, helper;
-  buffer += "\n	<div class=\"row-md-12 orderby-widget\">\n		<div class=\"col-md-6\">\n			<div class=\"onoffswitch\">\n    			<input type=\"checkbox\" name=\"onoffswitch\" class=\"onoffswitch-checkbox\" id=\"myonoffswitch\" ";
+  buffer += "\n		<div class=\"pull-right\">\n			<table>\n				<tr>\n					<td>\n						<div class=\"onoffswitch\">\n			    			<input type=\"checkbox\" name=\"onoffswitch\" class=\"onoffswitch-checkbox\" id=\"myonoffswitch\" ";
   if (helper = helpers.direction) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.direction); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + ">\n    			<label class=\"onoffswitch-label\" for=\"myonoffswitch\">\n        			<span class=\"onoffswitch-inner\"></span>\n       				 <span class=\"onoffswitch-switch\"></span>\n    			</label>\n			</div>\n		</div>\n		<div class=\"col-md-6\">\n			<label class=\"records\">";
+    + ">\n			    			<label class=\"onoffswitch-label\" for=\"myonoffswitch\">\n			        			<span class=\"onoffswitch-inner\"></span>\n			       				 <span class=\"onoffswitch-switch\"></span>\n			    			</label>\n						</div>\n					</td>\n					<td>\n						&nbsp;\n						<span style=\"font-size : x-large;\">";
   if (helper = helpers.limit) { stack1 = helper.call(depth0, {hash:{},data:data}); }
   else { helper = (depth0 && depth0.limit); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
   buffer += escapeExpression(stack1)
-    + " Records</label>\n		</div>\n	</div>\n	";
+    + "</span> <label class=\"records\">Records</label>\n					</td>\n				</tr>\n			</table>\n		</div>\n	";
   return buffer;
   }
 
