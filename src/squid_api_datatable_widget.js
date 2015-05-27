4f25814637becc68fc5b8f38678ab62ef61a8d86
Thomas Scott <thomas.scott@squidsolutions.com>
2015-05-27 11:27:57 +0200

    fixed non rollup ordering for datatable

--- src/squid_api_datatable_widget.js
+++ src/squid_api_datatable_widget.js
@@ -26,7 +26,7 @@
         
         rollupSummaryColumn : null,
 
-        ignoreColumns : null,
+        rollups : null,
 
         initialize : function(options) {
             var me = this;
@@ -70,9 +70,6 @@
             if (options.headerBadges) {
                 this.headerBadges = options.headerBadges;
             }
-            if (options.ignoreColumns) {
-                this.ignoreColumns = options.ignoreColumns;
-            }
             this.rollupSummaryColumn = options.rollupSummaryColumn;
 
             if (d3) {
@@ -103,23 +100,24 @@
         events : ({
             "click thead th" : function(item) {
                 if (this.ordering) {
-                    var orderByDirection = "ASC";
                     var orderId = parseInt($(item.currentTarget).attr("data-id"));
-                    this.$el.find(".sort-direction").remove();
-
-                    if (this.ignoreColumns) {
-                        orderId = orderId - this.ignoreColumns;
+                    var orderByDirection;
+                    if (this.rollups) {
+                        orderId = orderId - 1;
+                    } 
+                    if ($(item.currentTarget).hasClass("ASC")) {
+                        orderByDirection = "DESC";
+                    } else {
+                        orderByDirection = "ASC";
                     }
                     if (orderId === this.config.get("orderByColumn")) {
-                        if ($(item.currentTarget).hasClass("ASC")) {
-                            orderByDirection = "DESC";
-                        } else {
-                            orderByDirection = "ASC";
-                        }
                         this.config.set("orderByDirection", orderByDirection);
                     } else {
                         this.config.set("orderByColumn", orderId);
                     }
+
+                    console.log(orderId);
+                    console.log(orderByDirection);
                 }
             }
         }),
@@ -158,7 +156,9 @@
                 // init rollups
                 rollups = analysis.get("rollups");
                 if (rollups && (rollups.length ===0)) {
-                    rollups = null;
+                    rollups = this.rollups = null;
+                } else {
+                    this.rollups = true;
                 }
             } else {
                 // use analysis columns
@@ -208,14 +208,12 @@
             var orderBy = this.model.get("orderBy");
             for (col=0; col<columns.length; col++) {
                 for (ix=0; ix<orderBy.length; ix++) {
-                    if (col == orderBy[ix].col) {
-                        if (this.ignoreColumns) {
-                            columns[col + this.ignoreColumns].orderDirection = orderBy[ix].direction;
-                        } else {
-                            columns[col].orderDirection = orderBy[ix].direction;
-                        }
-                        break;
+                    if (this.rollups && col == orderBy[ix].col) {
+                        columns[col + 1].orderDirection = orderBy[ix].direction;
+                    } else if (col == orderBy[ix].col) {
+                        columns[col].orderDirection = orderBy[ix].direction;
                     }
+                    break;
                 }
             }
             
@@ -238,8 +236,8 @@
                     .data(columns)
                     .enter().append("th")
                     .attr("class", function(d, i) {
+                        var str = "";
                         if (rollups) {
-                            var str = "";
                             if (i === 0) {
                                 // hide grouping column
                                 str = str + "hide " + d.dataType;
@@ -249,11 +247,11 @@
                             } else {
                                 str = str + d.dataType;
                             }
-                            if (d.orderDirection) {
-                                str = str + " " + d.orderDirection;
-                            }
-                            return str;
                         }
+                        if (d.orderDirection) {
+                            str = str + " " + d.orderDirection;
+                        }
+                        return str;
                     })
                     .html(function(d, i) {
                         var str = d.name;
@@ -299,7 +297,9 @@
             if (results) {
                 rollups = analysis.get("rollups");
                 if (rollups && (rollups.length ===0)) {
-                    rollups = null;
+                    rollups = this.rollups = null;
+                } else {
+                    this.rollups = true;
                 }
                 
                 var rollupColIndex = null;
