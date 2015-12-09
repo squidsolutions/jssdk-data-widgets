373f86b6174faae594b525959cd7fa0e45e9b0e9
Louis Rabiet <louis.rabiet@squidsolutions.com>
2015-12-09 17:16:48 +0100

    sub templates files

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,14 +15,44 @@ module.exports = function(grunt) {
                     options : {
                         stripBanners : true,
                     },
-                    css : {
-                        src : ['src/*.css' ],
-                        dest : 'dist/squid_api_data-widgets.css',
-                    },
-                    js : {
-                        src : [ 'build/templates.js',
+
+                        css : {
+                            src : ['src/*.css' ],
+                            dest : 'dist/squid_api_data-widgets.css',
+                        },
+                        js : {
+                            src : [ 'build/templates.js',
                                 'src/*.js' ],
-                        dest : 'dist/squid_api_data-widgets.js',
+                            dest : 'dist/squid_api_data-widgets.js',
+
+                    },
+                    dev:{
+                        files: {
+                            'dist/squid_api_barchart_widget.js': ['dist/squid_api_barchart_widget_template.js','src/squid_api_barchart_widget.js'],
+                            'dist/squid_api_datatable_widget.js': ['dist/squid_api_datatable_widget_template.js','src/squid_api_datatable_widget.js'],
+                            'dist/squid_api_dimension_selector_widget.js': ['dist/squid_api_dimension_selector_widget_template.js', 'src/squid_api_dimension_selector_widget.js'],
+                            'dist/squid_api_dimension_widget.js': ['dist/squid_api_dimension_widget_template.js', 'src/squid_api_dimension_widget.js'],
+                            'dist/squid_api_displaytype_selector_widget.js': ['dist/squid_api_displaytype_selector_widget_template.js', 'src/squid_api_displaytype_selector_widget.js'],
+                            'dist/squid_api_domain_selector_widget.js': ['dist/squid_api_domain_selector_widget_template.js', 'src/squid_api_domain_selector_widget.js'],
+                            'dist/squid_api_export_scheduler_widget.js': ['dist/squid_api_export_scheduler_widget_template.js', 'src/squid_api_export_scheduler_widget.js'],
+                            'dist/squid_api_kpi_widget.js': ['dist/squid_api_kpi_widget_template.js', 'src/squid_api_kpi_widget.js'],
+                            'dist/squid_api_metric_selector_widget.js': ['dist/squid_api_metric_selector_widget_template.js', 'src/squid_api_metric_selector_widget.js'],
+                            'dist/squid_api_metric_total_widget.js': ['dist/squid_api_metric_total_widget_template.js', 'src/squid_api_metric_total_widget.js'],
+                            'dist/squid_api_metric_widget.js': ['dist/squid_api_metric_widget_template.js', 'src/squid_api_metric_widget.js'],
+                            'dist/squid_api_orderby_widget.js': ['dist/squid_api_orderby_widget_template.js', 'src/squid_api_orderby_widget.js'],
+                            'dist/squid_api_project_selector_widget.js': ['dist/squid_api_project_selector_widget_template.js', 'src/squid_api_project_selector_widget.js'],
+                            'dist/squid_api_timeseries_widget.js': ['dist/squid_api_timeseries_widget_template.js', 'src/squid_api_timeseries_widget.js'],
+                        },
+                    }
+                },
+                copy: {
+                    devDist: {
+                        files: [{
+                            expand: true,
+                            flatten: true,
+                            src : [ 'src/*.js' ],
+                            dest: 'dist'
+                        }]
                     }
                 },
                 sass: {
@@ -47,6 +77,24 @@ module.exports = function(grunt) {
                         files : {
                             "build/templates.js" : [ "src/*.hbs" ]
                         }
+                    },
+                    dev : {
+                        files : {
+                            'dist/squid_api_barchart_widget_template.js': ['src/squid_api_barchart_widget.hbs'],
+                            'dist/squid_api_datatable_widget_template.js': ['src/squid_api_datatable_widget.hbs'],
+                            'dist/squid_api_dimension_selector_widget_template.js': ['src/squid_api_dimension_selector_widget.hbs'],
+                            'dist/squid_api_dimension_widget_template.js': ['src/squid_api_dimension_widget.hbs'],
+                            'dist/squid_api_displaytype_selector_widget_template.js': ['src/squid_api_displaytype_selector_widget.hbs'],
+                            'dist/squid_api_domain_selector_widget_template.js': ['src/squid_api_domain_selector_widget.hbs'],
+                            'dist/squid_api_export_scheduler_widget_template.js': ['src/squid_api_export_scheduler_widget.hbs'],
+                            'dist/squid_api_kpi_widget_template.js': ['src/squid_api_kpi_widget.hbs'],
+                            'dist/squid_api_metric_selector_widget_template.js': ['src/squid_api_metric_selector_widget.hbs'],
+                            'dist/squid_api_metric_total_widget_template.js': ['src/squid_api_metric_total_widget.hbs'],
+                            'dist/squid_api_metric_widget_template.js': ['src/squid_api_metric_widget.hbs'],
+                            'dist/squid_api_orderby_widget_template.js': ['src/squid_api_orderby_widget.hbs'],
+                            'dist/squid_api_project_selector_widget_template.js': ['src/squid_api_project_selector_widget.hbs'],
+                            'dist/squid_api_timeseries_widget_template.js': ['src/squid_api_timeseries_widget.hbs'],
+                        }
                     }
                 },
                 watch : {
@@ -62,6 +110,9 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-handlebars');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask('default', [ 'jshint', 'clean', 'handlebars', 'concat']);
+    grunt.registerTask('devDist', [ 'jshint', 'clean', 'handlebars:dev', 'copy:devDist', 'concat:dev' ]);
+
+    grunt.registerTask('default', [ 'jshint', 'clean', 'handlebars', 'concat:js', 'concat:css']);
 };
