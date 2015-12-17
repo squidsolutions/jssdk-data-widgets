07efc20e3066321c79644d18c310807fddd02a62
Thomas Scott <thomas.scott@squidsolutions.com>
2015-12-17 18:05:58 +0100

    add defaultBehaviour & listen to period change in filters controller

--- dist/squid_api_data-widgets.js
+++ dist/squid_api_data-widgets.js
@@ -3302,6 +3302,8 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
         onChangeHandler : null,
 
         initialize: function(options) {
+            this.config = squid_api.model.config;
+
             if (this.model) {
                 this.filters = this.model;
             } else {
@@ -3309,27 +3311,35 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
             }
 
             if (options) {
-                // setup options
-                if (options.config) {
-                    this.config = options.config;
-                }
                 this.onChangeHandler = options.onChangeHandler;
             }
 
-            if (!this.config) {
-                this.config = squid_api.model.config;
-            }
-
             // check for new filter selection made by config update
-            this.listenTo(this.config, 'change:selection', function() {
-                this.initFilters(this.config);
-            });
+            this.listenTo(this.config, 'change:selection', this.initFilters);
+            // check for updated period made by config
+            this.listenTo(this.config, 'change:period', this.resetPeriodSelection);
         },
 
-        initFilters : function(config) {
+        resetPeriodSelection: function() {
+            var selection = this.filters.get("selection");
+            var domain = this.config.get("domain");
+            var periodConfig = this.config.get("period");
+            if (selection) {
+                if (selection.facets) {
+                    for (i=0; i<selection.facets.length; i++) {
+                        if (selection.facets[i].dimension.type === "DATE" && selection.facets[i].id !== periodConfig[domain]) {
+                            selection.facets.splice(i, 1);
+                        }
+                    }
+                    this.config.set("selection", selection);
+                }
+            }
+        },
+
+        initFilters : function() {
             var me = this;
-            var domainId = config.get("domain");
-            var projectId = config.get("project");
+            var domainId = this.config.get("domain");
+            var projectId = this.config.get("project");
 
             if (projectId && domainId) {
                 var domainPk = {
@@ -3346,7 +3356,7 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
                 filters.setDomainIds([domainPk]);
 
                 console.log("compute (initFilters)");
-                $.when(squid_api.controller.facetjob.compute(filters, config.get("selection")))
+                $.when(squid_api.controller.facetjob.compute(filters, this.config.get("selection")))
                 .then(function() {
                     // search for time facets and make such they are done
                     var timeFacets = [];
@@ -3380,12 +3390,71 @@ helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
             }
         },
 
+        defaultBehaviour: function(selection, timeFacets) {
+            var configPeriod = this.config.get("period");
+            var domain = this.config.get("domain");
+            var timeFacet = false;
+            if (configPeriod) {
+                if (configPeriod[domain]) {
+                    for (i=0; i<timeFacets.length; i++) {
+                        if (configPeriod[domain] === timeFacets[i].oid) {
+                            timeFacet = timeFacets[i];
+                        }
+                        break;
+                    }
+                }
+            }
+            if (!timeFacet) {
+                for (i=0; i<timeFacets.length; i++) {
+                    timeFacet = timeFacets[i];
+                    break;
+                }
+            }
+            if (timeFacet) {
+                // set config period
+                if (! configPeriod) {
+                    var obj = {};
+                    obj[domain] = timeFacet.id;
+                    this.config.set("period", obj);
+                } else {
+                    configPeriod[domain] = timeFacet.id;
+                    this.config.set("period", configPeriod);
+                }
+                // set selectedItems
+                if (timeFacet.selectedItems.length === 0) {
+                    var minDate;
+                    var maxDate;
+                    // detect mix & max
+                    if (timeFacet.items.length > 0) {
+                        minDate = timeFacet.items[0].lowerBound;
+                        maxDate = timeFacet.items[0].upperBound;
+                    } else {
+                        minDate = moment().subtract("50", "years").format(squid_api.DATE_FORMAT);
+                        maxDate = moment().format(squid_api.DATE_FORMAT);
+                    }
+                    // set timeFacet selected Items
+                    timeFacet.selectedItems = [{upperBound : maxDate, lowerBound : moment(maxDate).subtract("1", "month").format(squid_api.DATE_FORMAT), type : "i"}];
+                    // update selection
+                    for (ix=0; ix<selection.facets.length; ix++) {
+                        if (selection.facets[ix].id === timeFacet.id) {
+                            selection.facets[ix] = timeFacet;
+                        }
+                    }
+                    // set selection in config
+                    this.config.set("selection", squid_api.utils.buildCleanSelection(selection));
+                } else {
+                    this.filters.set("selection", selection);
+                }
+            } else {
+                this.filters.set("selection", selection);
+            }
+        },
+
         changed : function(selection, timeFacets) {
             if (this.onChangeHandler) {
                 this.onChangeHandler(selection, timeFacets);
             } else {
-                // default behavior
-                this.filters.set("selection", selection);
+                this.defaultBehaviour(selection, timeFacets);
             }
         }
 
