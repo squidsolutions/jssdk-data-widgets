fab3cba1acfdcd375e1989d14be784b7ec7116dc
olivier <obalbous@squidsolutions.com>
2016-01-15 17:58:17 +0100

    moved resetPeriodSelection to FiltersWidget, added comments and changed delegation

--- src/squid_api_filters_controller.js
+++ src/squid_api_filters_controller.js
@@ -24,36 +24,6 @@
 
             // check for new filter selection made by config update
             this.listenTo(this.config, 'change:selection', this.initFilters);
-            // check for updated period made by config
-            this.listenTo(this.config, 'change:period', function() {
-                this.resetPeriodSelection();
-            });
-        },
-
-        resetPeriodSelection: function() {
-            /*  responsible for removing a previously active date
-                facet from the current selection.
-             */
-            var selection = $.extend(true, {}, this.config.get("selection"));
-            var domain = this.config.get("domain");
-            var periodConfig = this.config.get("period");
-            if (selection) {
-                var facets = selection.facets;
-                if (facets) {
-                    var changed = false;
-                    for (var i=0; i<facets.length; i++) {
-                        var facet = facets[i];
-                        if (facet.dimension.type === "CONTINUOUS" && facet.dimension.valueType === "DATE") {
-                            if (facet.id !== periodConfig[domain]) {
-                                changed = true;
-                                facets.splice(i, 1);
-                            }
-                        }
-                    }
-                    selection.facets = facets;
-                    this.config.set("selection", selection);
-                }
-            }
         },
 
         initFilters : function() {
@@ -81,7 +51,7 @@
                 .always(function() {
                     // update global filters
                     me.filters.set({"domains": filters.get("domains"), "id" : filters.get("id")}, {"silent" : true});
-                    // search for time facets and make such they are done
+                    // search for time facets
                     var sel = filters.get("selection");
                     if (sel && sel.facets) {
                         var facets = sel.facets;
@@ -91,32 +61,34 @@
                                 timeFacets.push(facet);
                             }
                         }
-                    }    
+                    }
+                    // delegate further processing
                     me.changed(filters.get("selection"), timeFacets);
                 });
             }
         },
 
-        defaultBehaviour: function(selection, timeFacets) {
-            /*  responsible providing default behavoir in order to:
-                1. detecting a time facet to set the config
-                2. automatically selecting a currently active facet range
-                3. setting the facet selection
-             */
+        /**
+         *   responsible providing default behavoir in order to:
+         *   1. detecting a time facet to set the config
+         *   2. automatically selecting a currently active facet range
+         *   3. setting the facet selection
+         */
+        changed: function(selection, timeFacets) {
             var configPeriod = this.config.get("period");
             var domain = this.config.get("domain");
-            var timeFacet = false;
-            if (configPeriod) {
-                if (configPeriod[domain]) {
-                    for (i=0; i<timeFacets.length; i++) {
-                        if (configPeriod[domain] === timeFacets[i].id && ! timeFacets[i].error) {
-                            timeFacet = timeFacets[i];
-                            break;
-                        }
+            var timeFacet = null;
+            if (configPeriod && configPeriod[domain]) {
+                // pick the time facet corresponding to the selected period
+                for (i=0; i<timeFacets.length; i++) {
+                    if (configPeriod[domain] === timeFacets[i].id && ! timeFacets[i].error) {
+                        timeFacet = timeFacets[i];
+                        break;
                     }
                 }
             }
             if (!timeFacet) {
+                // pick the first time facet
                 for (i=0; i<timeFacets.length; i++) {
                     if (timeFacets[i].dimension.valueType === "DATE" && timeFacets[i].dimension.type === "CONTINUOUS"  && ! timeFacets[i].error) {
                         timeFacet = timeFacets[i];
@@ -125,7 +97,7 @@
                 }
             }
             if (timeFacet) {
-                // set config period
+                // set period to the selected timefacet
                 if (configPeriod) {
                     if (configPeriod[domain]) {
                         if (configPeriod[domain] !== timeFacet.id) {
@@ -139,47 +111,44 @@
                     obj[domain] = timeFacet.id;
                     configPeriod = obj;
                 }
-
-                // set the default period
                 this.config.set("period", configPeriod);
-
-                // set selectedItems
-                if (timeFacet.selectedItems.length === 0) {
-                    var minDate;
-                    var maxDate;
-                    // detect mix & max
-                    if (timeFacet.items.length > 0) {
-                        minDate = timeFacet.items[0].lowerBound;
-                        maxDate = timeFacet.items[0].upperBound;
-                    } else {
-                        minDate = moment().subtract("50", "years").startOf("day").format(squid_api.DATE_FORMAT);
-                        maxDate = moment().endOf("day").format(squid_api.DATE_FORMAT);
-                    }
-                    // set timeFacet selected Items
-                    timeFacet.selectedItems = [{upperBound : maxDate, lowerBound : moment(maxDate).subtract("1", "month").format(squid_api.DATE_FORMAT), type : "i"}];
-                    // update selection
-                    for (ix=0; ix<selection.facets.length; ix++) {
-                        if (selection.facets[ix].id === timeFacet.id) {
-                            selection.facets[ix] = timeFacet;
-                        }
-                    }
-                    // set selection in config
-                    this.config.set("selection", squid_api.utils.buildCleanSelection(selection));
+                
+                if (this.onChangeHandler) {
+                    this.onChangeHandler(selection, timeFacets);
                 } else {
-                    this.filters.set("selection", selection);
-                }  
+                    // defaultBehaviour
+                    if (timeFacet.selectedItems.length === 0) {
+                        // no date range selected : apply default time selection rules
+                        var minDate;
+                        var maxDate;
+                        if (timeFacet.items.length > 0) {
+                            minDate = timeFacet.items[0].lowerBound;
+                            maxDate = timeFacet.items[0].upperBound;
+                        } else {
+                            // if no min and max, arbitrary select a 50 years range
+                            minDate = moment().subtract("50", "years").startOf("day").format(squid_api.DATE_FORMAT);
+                            maxDate = moment().endOf("day").format(squid_api.DATE_FORMAT);
+                        }
+                        // select a one month date range
+                        timeFacet.selectedItems = [{
+                            upperBound : maxDate, 
+                            lowerBound : moment(maxDate).subtract("1", "month").format(squid_api.DATE_FORMAT), 
+                            type : "i"}];
+                        for (ix=0; ix<selection.facets.length; ix++) {
+                            if (selection.facets[ix].id === timeFacet.id) {
+                                selection.facets[ix] = timeFacet;
+                            }
+                        }
+                        // apply selection to config (will trigger new facet computation)
+                        this.config.set("selection", squid_api.utils.buildCleanSelection(selection));
+                    } else {
+                        this.filters.set("selection", selection);
+                    }
+                }
             } else {
                 this.filters.set("selection", selection);
             }
         },
-
-        changed : function(selection, timeFacets) {
-            if (this.onChangeHandler) {
-                this.onChangeHandler(selection, timeFacets);
-            } else {
-                this.defaultBehaviour(selection, timeFacets);
-            }
-        }
 
     });
 
